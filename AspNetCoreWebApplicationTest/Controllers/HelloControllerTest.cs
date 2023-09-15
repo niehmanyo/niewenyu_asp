7f5a58f0783c5ee7e64a8516170f89491473445c
Russell Nie <92361244+niehmanyo@users.noreply.github.com>
2023-09-15 10:34:40 +0800

    Create HelloControllerTest.cs

---
+++ AspNetCoreWebApplicationTest/Controllers/HelloControllerTest.cs
@@ -0,0 +1,8 @@
+ [Fact]
+        public void NoInputParamGetResponseTest()
+        {
+            HelloController controller = new HelloController();
+            var response = controller.Get().Value as Response;
+            Assert.Equal("Hello World!!!", response.output);
+        }
+

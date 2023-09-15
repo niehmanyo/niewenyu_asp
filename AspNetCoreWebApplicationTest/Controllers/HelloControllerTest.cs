56ef40dbb1513d5e15c2aceb8dd11cabf75230f0
Russell Nie <92361244+niehmanyo@users.noreply.github.com>
2023-09-15 10:35:26 +0800

    Update HelloControllerTest.cs

--- AspNetCoreWebApplicationTest/Controllers/HelloControllerTest.cs
+++ AspNetCoreWebApplicationTest/Controllers/HelloControllerTest.cs
@@ -1,8 +1,18 @@
- [Fact]
+using AspNetCoreWebApplication.Controllers;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebApplicationTest.Controllers
+{
+    public class HelloControllerTest
+    {
+        [Fact]
         public void NoInputParamGetResponseTest()
         {
             HelloController controller = new HelloController();
             var response = controller.Get().Value as Response;
             Assert.Equal("Hello World!!!", response.output);
         }
-
+      
+    }
+}
